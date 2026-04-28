b1c5b15140b335a031f1506a51f7816418e97865
SamuelPatrickMeneses <samuelmeneses800@gmail.com>
2026-04-28 14:12:01 -0300

    type: modify env files: - db/Dockerfile - db/pb_data/.gitkeep - db/startup.sh - docker-compose.yml - package.json - server/nginx.conf description: modify the enviromento to auto buil and serve the content.

---
+++ db/startup.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# Upsert superuser se as variáveis estiverem definidas
+if [ -n "$PB_SUPERUSER_EMAIL" ] && [ -n "$PB_SUPERUSER_PASSWORD" ]; then
+
+    echo "Configurando superusuário PocketBase..."
+    
+    pocketbase superuser upsert "$PB_SUPERUSER_EMAIL" "$PB_SUPERUSER_PASSWORD" \
+    --http=0.0.0.0:8090 \
+    --dir /app/pb_data \
+    --hooksDir /app/pb_hooks \
+    --migrationsDir /app/pb_migrations \
+    --publicDir /app/pb_public
+
+fi
+id
+ls -la /app/pb_data
+# Inicia o servidor PocketBase
+echo "Iniciando PocketBase..."
+
+pocketbase serve \
+  --http=0.0.0.0:8090 \
+  --dir /app/pb_data \
+  --hooksDir /app/pb_hooks \
+  --migrationsDir /app/pb_migrations \
+  --publicDir /app/pb_public

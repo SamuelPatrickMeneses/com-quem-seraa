033b422471fc14e842b1739744603d988d8d2750
SamuelPatrickMeneses <samuelmeneses800@gmail.com>
2026-04-29 03:00:31 -0300

    type: chnage db files: - db/pb_migrations/1777441546_created_groups.js - db/pb_migrations/1777441802_created_group_participants.js - db/pb_migrations/1777441879_updated_groups.js - db/pb_migrations/1777441939_collections_snapshot.js description: add db entitys.

---
+++ db/pb_migrations/1777441879_updated_groups.js
@@ -0,0 +1,28 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("pbc_3346940990")
+
+  // update collection data
+  unmarshal({
+    "createRule": "@request.auth.id != \"\"",
+    "deleteRule": "created_by = @request.auth.id",
+    "listRule": "@request.auth.id != \"\"",
+    "updateRule": "created_by = @request.auth.id",
+    "viewRule": "@request.auth.id != \"\""
+  }, collection)
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("pbc_3346940990")
+
+  // update collection data
+  unmarshal({
+    "createRule": null,
+    "deleteRule": null,
+    "listRule": null,
+    "updateRule": null,
+    "viewRule": null
+  }, collection)
+
+  return app.save(collection)
+})

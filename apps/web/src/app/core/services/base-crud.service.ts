a3a012341b426d66fc96cb82c10fa3ef7ee39ef7
GabrielKuiawa <gabrielsantos77239@gmail.com>
2026-06-02 00:31:01 -0300

    feat: implement PocketBase client and generic CRUD service for shared database access

---
+++ apps/web/src/app/core/services/base-crud.service.ts
@@ -0,0 +1,33 @@
+import { PocketBaseClient } from '../../infrastructure/pocketbase/pocketbase.client';
+import { RecordModel, RecordService } from 'pocketbase';
+
+export abstract class BaseCrudService<T> {
+  protected collection: RecordService;
+
+  constructor(
+    protected pbClient: PocketBaseClient,
+    protected collectionName: string
+  ) {
+    this.collection = this.pbClient.instance.collection(this.collectionName);
+  }
+
+  async getList(page = 1, perPage = 50, options?: any) {
+    return await this.collection.getList<T & RecordModel>(page, perPage, options);
+  }
+
+  async getById(id: string, options?: any) {
+    return await this.collection.getOne<T & RecordModel>(id, options);
+  }
+
+  async create(data: Partial<T>, options?: any) {
+    return await this.collection.create<T & RecordModel>(data, options);
+  }
+
+  async update(id: string, data: Partial<T>, options?: any) {
+    return await this.collection.update<T & RecordModel>(id, data, options);
+  }
+
+  async delete(id: string, options?: any) {
+    return await this.collection.delete(id, options);
+  }
+}

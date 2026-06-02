39de70be8f2c3511b31043b59a67497970d4f86c
GabrielKuiawa <gabrielsantos77239@gmail.com>
2026-06-02 00:31:01 -0300

    feat: implement PocketBase client and generic CRUD service for shared database access

--- apps/web/src/app/core/services/auth.service.ts
+++ apps/web/src/app/core/services/auth.service.ts
@@ -1,50 +1,47 @@
 import { Injectable } from '@angular/core';
 import PocketBase from 'pocketbase';
+import { PocketBaseClient } from '../../infrastructure/pocketbase/pocketbase.client';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  private pb: PocketBase;
-
-  constructor() {
-    this.pb = new PocketBase('http://127.0.0.1:80');
-  }
+  constructor(private pbClient: PocketBaseClient) {}
 
   /**
    * Retorna se o usuário está autenticado
    */
   get isAuthenticated(): boolean {
-    return this.pb.authStore.isValid;
+    return this.pbClient.instance.authStore.isValid;
   }
 
   /**
    * Retorna os dados do usuário logado
    */
   get user() {
-    return this.pb.authStore.model;
+    return this.pbClient.instance.authStore.model;
   }
 
   /**
    * Realiza o login com e-mail e senha
    */
   async login(email: string, pass: string) {
-    return await this.pb.collection('users').authWithPassword(email, pass);
+    return await this.pbClient.instance.collection('users').authWithPassword(email, pass);
   }
 
   /**
    * Realiza o logout
    */
   logout() {
-    this.pb.authStore.clear();
+    this.pbClient.instance.authStore.clear();
   }
 
   /**
    * Realiza o registro de um novo usuário
    */
   async register(data: any) {
-    const user = await this.pb.collection('users').create(data);
-    await this.pb.collection('users').requestVerification(data.email);
+    const user = await this.pbClient.instance.collection('users').create(data);
+    await this.pbClient.instance.collection('users').requestVerification(data.email);
     return user;
   }
 
@@ -52,6 +49,6 @@ export class AuthService {
    * Retorna a instância do PocketBase para outros usos (ex: listar coleções)
    */
   get pocketBase(): PocketBase {
-    return this.pb;
+    return this.pbClient.instance;
   }
 }

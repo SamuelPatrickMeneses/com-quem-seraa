1e384d24fdeedeac7db272d32e75867d57eb9447
SamuelPatrickMeneses <samuelmeneses800@gmail.com>
2026-06-12 11:58:27 -0300

    type: test and db files: db/pb_hooks/seed.pb.js description: add a cleaning route.

--- db/pb_hooks/seed.pb.js
+++ db/pb_hooks/seed.pb.js
@@ -1,5 +1,4 @@
 /// <reference path="../pb_data/types.d.ts" />
-
 onBootstrap((e) => {
     console.log("Seed: Hook onBootstrap disparado.");
 
@@ -73,3 +72,60 @@ onBootstrap((e) => {
         console.error("Seed Erro Detalhado:", err);
     }
 })
+
+
+const env = (typeof $os !== "undefined" && $os.getenv) ? $os.getenv("APP_ENV") : "";
+if (env === "dev") {
+    routerAdd("GET", "/api/test/clear", (e) => {
+        
+        try {
+            // Tenta acessar variáveis de ambiente de forma resiliente
+            $app.db().newQuery("DELETE FROM group_participants").execute()
+            $app.db().newQuery("DELETE FROM groups").execute()
+            $app.db().newQuery("DELETE FROM users").execute()
+            const usersCollection = $app.findCollectionByNameOrId("users");
+            const groupsCollection = $app.findCollectionByNameOrId("groups");
+            const participantsCollection = $app.findCollectionByNameOrId("group_participants");
+
+            if (!groupsCollection || !participantsCollection) {
+                console.log("Seed: Coleções 'groups' ou 'group_participants' ainda não existem.");
+                return;
+            }
+
+
+            const testUserEmails = ["ana@exemplo.com", "beto@exemplo.com", "caio@exemplo.com"];
+            const userIds = [];
+
+
+            for (const email of testUserEmails) {
+                // Se chegou aqui, o usuário não existe. Cria um novo.
+                const record = new Record(usersCollection);
+                record.set("email", email);
+                record.set("name", email.split("@")[0]);
+                record.set("password", "1234567890");
+                record.set("verified", true);
+                $app.save(record);
+                userIds.push(record.id);
+            }
+
+            // Verifica se já existe um grupo criado por esses usuários
+            const groupRecord = new Record(groupsCollection);
+            groupRecord.set("name", "Amigo Secreto 2024");
+            groupRecord.set("created_by", userIds[0]);
+            groupRecord.set("participants_count", 3);
+            $app.save(groupRecord);
+
+            for (const userId of userIds) {
+                const partRecord = new Record(participantsCollection);
+                partRecord.set("group_id", groupRecord.id);
+                partRecord.set("giver_id", userId);
+                $app.save(partRecord);
+            }
+
+            return e.json(200, {message: "Seed: Sucesso na inserção dos dados de teste!"});
+        } catch (err) {
+            return e.json(500, {message: "Seed Erro Detalhado:", error: err});
+        }
+    });
+}
+
